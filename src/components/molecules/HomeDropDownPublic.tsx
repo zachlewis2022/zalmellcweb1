8bf3862879c9c6053404d8db69ec42a9e607b034
devcmgm <49372756+devcmgm@users.noreply.github.com>
2022-09-18 11:10:53 -0500

    new

--- src/components/molecules/HomeDropDownPublic.tsx
+++ src/components/molecules/HomeDropDownPublic.tsx
@@ -6,15 +6,14 @@ export const HomeDropDownPublic = () => {
       <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
        
-        <span className="fs-4">Simple header</span>
+        <span className="fs-4">Zachary Lewis and Associates</span>
       </a>
 
       <ul className="nav nav-pills">
-        <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">About</a></li>
+        <li className="nav-item"><a href="#" className="nav-link" aria-current="page">Home</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Our Team</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Our Companies</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Our Story</a></li>
       </ul>
     </header>
     </div>
